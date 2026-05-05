e5783049c4d81572a82e551bb9862b2098eeb7dd
amiralinmp841-alt <amiralinmp841@gmail.com>
2026-05-05 22:23:37 +0330

    Update vip.py

--- vip.py
+++ vip.py
@@ -433,7 +433,7 @@ def process_local_file(bale_chat_id, file_bytes, file_name, mime_type=None, capt
 
     bale_send_text(bale_chat_id, "✅ ارسال کامل شد.")
 
-def split_video_ffmpeg_vip(input_path, bale_chat_id, max_part_mb=10):
+def split_video_ffmpeg_vip(input_path, bale_chat_id, max_part_mb=5):
     import os, subprocess, tempfile, shutil
 
     MAX_BYTES = max_part_mb * 1024 * 1024
